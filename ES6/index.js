0afe218e29f1b8cb34a90af84d8366370ac9bf69
Noman-1533 <abu.noman@brainstation-23.com>
2024-05-03 10:38:32 +0600

    Updated at 3rd May 2024

---
+++ ES6/index.js
@@ -0,0 +1,11 @@
+// import { sum, sub  } from './math'
+// math=require('./math')
+console.log('Hello Webpack 4')
+
+// console.log(sum(10, 20))
+// console.log(sub(10, 20))
+
+// alert('Hello Catians')
+var name = "Md Abu Noman";
+var age = 25;
+console.log(`Hello my name is ${name} and i am ${age} years old`)
